01bb9030f8abb28da048289322296350cd7d5219
Nadezhda Batyuchenko <n.batyuchenko@office.ngs.ru>
2017-07-12 23:32:51 +0700

    storage desc

--- storage/storage.go
+++ storage/storage.go
@@ -8,7 +8,7 @@ type Assignment struct {
 	userId   int
 	itemName string
 }
-
+// !!! одна копия!
 type Assignments []Assignment
 
 var assignments Assignments
@@ -31,9 +31,24 @@ func Refresh() {
 
 func getFromDb() Assignments {
 	// implement loading from db
+	// get by user
 
 	a := Assignment{userId: 123, itemName: "123_name"}
 	b := Assignment{userId: 321, itemName: "321_name"}
 
 	return Assignments{a, b}
 }
+
+/*
+- user assignments: roles list
+- check access recursively
+
+- auth_assignments, auth_item, auth_item_child
+All auth data:
+1. From auth_item: items => item, ...
+2. From auth_item_child: parents[child][] => parent, ...
+3. From auth_assignments: assignments[user][item name] => assignment
+
+Грузить все данные не нужно в storage, нужны данныые только для пользователя
+
+ */
